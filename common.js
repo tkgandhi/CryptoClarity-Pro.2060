48967a94a160c4802330f4d0de2bdf44d30aee8e
tkgandhi <109563744+tkgandhi@users.noreply.github.com>
2026-05-17 14:37:54 -0400

    Add files via upload

---
+++ common.js
@@ -0,0 +1,43 @@
+function home() {
+    window.location = "index.html";
+}
+
+function intro() {
+    window.location = "intro.html";
+}
+
+function purpose() {
+    window.location = "purpose.html";
+}
+
+function dlt() {
+    window.location = "dlt.html";
+}
+
+function history() {
+    window.location = "history.html";
+}
+
+function types() {
+    window.location = "types.html";
+}
+
+function risks() {
+    window.location = "risks.html";
+}
+
+function quiz() {
+    window.location = "quiz.html";
+}
+
+function signup() {
+    window.location = "signup.html";
+}
+
+function gloss() {
+    window.location = "gloss.html";
+}
+
+function extra() {
+    window.location = "about.html";
+}
